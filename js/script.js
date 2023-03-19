ae2c5483293234666ba4b6867ad96d7ab2c286ce
Lusine679 <67959081+Lusine679@users.noreply.github.com>
2023-03-19 16:22:44 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+jQuery('.close-menu').click(function(){
+    jQuery("body").css({"overflow": "auto"});
+});
+jQuery(".m-menu__overlay").click(function(){
+    jQuery("body").css({"overflow": "auto"});
+});
+jQuery('.open-btn').click(function(){
+    jQuery("body").css({"overflow": "hidden"});
+});
